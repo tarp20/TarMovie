30181c7498ddb56db92466e6e246d55080a1bb13
Taras Piont <taras.piont@gmail.com>
2021-01-29 16:07:29 +0100

    2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import PropTypes from 'prop-types';
+
+function Food({name,picture,rating}){
+    return (
+      <div>
+        <h3>I like {name}</h3>
+        <h4>Rating {rating}/5</h4>
+        <img src={picture} alt=""/>
+      </div>
+    );
+}
+
+
+
+
+const foodILike = [
+  {
+  'id': 1,
+  'name': 'pizza',
+  'image': 'https://www.kwestiasmaku.com/sites/v123.kwestiasmaku.com/files/ciasto_na_pizze_00.jpg',
+  'rating': '4.5'
+  },
+  {
+  'id': 2,
+  'name': 'pasta',
+  'image': 'https://www.saltandlavender.com/wp-content/uploads/2020/04/tomato-goat-cheese-pasta-recipe-1-500x500.jpg',
+  'rating': '4.2'
+  },
+  {
+  'id': 3,
+  'name': 'risotto',
+  'image': 'https://www.tasteofhome.com/wp-content/uploads/2018/01/Parmesan-Risotto_EXPS_HPLBZ18_33569_B05_18_5b.jpg',
+  'rating': '4.0'
+  }
+]
+Food.propTypes={
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+}
+
+function renderFood(dish){
+  return <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
+
+}
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {foodILike.map(renderFood)}
     </div>
   );
 }
 
 export default App;
+
